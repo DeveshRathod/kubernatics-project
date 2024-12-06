3c9710eb1e32db5b6f2a73fd3ed78c51e76e391e
Devesh Rathod <deveshrathod@Deveshs-MacBook-Air.local>
2024-12-06 17:15:26 +0530

    done

--- main-ec2/rds.tf
+++ main-ec2/rds.tf
@@ -1,21 +1,21 @@
 resource "aws_db_instance" "rds" {
-  allocated_storage      = 20
-  identifier = "book-rds"
-  db_subnet_group_name   = aws_db_subnet_group.sub-grp.id
-  engine                 = "mysql"
-  engine_version         = "8.0.32"
-  instance_class         = "db.t3.micro"
-  multi_az               = true
-  db_name                = "mydb"
-  username               = "admin"
-  password               = "srivardhan"
-  skip_final_snapshot    = true
-  vpc_security_group_ids = [aws_security_group.security-group.id]
-  depends_on = [ aws_db_subnet_group.sub-grp ]
-  publicly_accessible = true
+  allocated_storage       = 20
+  identifier              = "book-rds"
+  db_subnet_group_name    = aws_db_subnet_group.sub-grp.id
+  engine                  = "mysql"
+  engine_version          = "8.0.32"
+  instance_class          = "db.t3.micro"
+  multi_az                = true
+  db_name                 = "mydb"
+  username                = "<username"
+  password                = "<password>"
+  skip_final_snapshot     = true
+  vpc_security_group_ids  = [aws_security_group.security-group.id]
+  depends_on              = [aws_db_subnet_group.sub-grp]
+  publicly_accessible     = true
   backup_retention_period = 7
 
-  
+
   tags = {
     DB_identifier = "book-rds"
   }
--- main-ec2/terraform.tf
+++ main-ec2/terraform.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "deveshawsdevv"
+    bucket = "deveshawsdevvvv"
     key    = "ec2/terraform.tfstate"
     region = "us-east-1"
   }
